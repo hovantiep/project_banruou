aaf04182b5437fb11ee411d87de704068264d4d1
Van Tiep <hovantiep1989@gmail.com>
2016-04-14 22:42:09 +0700

    product edit

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -2,24 +2,52 @@
 @section('controller','Product')
 @section('action','Edit')
 @section('content')
+    <style>
+        .img-thumbnail-current {
+            max-width: 250px;
+        }
 
-    <div class="col-lg-7" style="padding-bottom:120px">
-        <form action="" method="POST">
+        .img-thumbnail-detail {
+            max-width: 200px;
+        }
+    </style>
+    @include('admin.block.vali_msg')
+    <form action="" method="POST">
+        <div class="col-lg-7" style="padding-bottom:120px">
+            <div class="form-group">
+                <label>Category</label>
+                <select class="form-control" name="sltCate">
+                    <option value="0">Please Choose Category</option>
+                    {!! cate_parent($cate, 0, "", $product->cate_id) !!}
+                </select>
+            </div>
             <div class="form-group">
                 <label>Name</label>
-                <input class="form-control" name="txtName" placeholder="Please Enter Username"/>
+                <input class="form-control" name="txtName" placeholder="Please Enter Username"
+                       value="{!! old('txtName',$product->name) !!}"/>
             </div>
             <div class="form-group">
                 <label>Price</label>
-                <input class="form-control" name="txtPrice" placeholder="Please Enter Password"/>
+                <input class="form-control" name="txtPrice" placeholder="Please Enter Password"
+                       value="{!! old('txtPrice',$product->price) !!}"/>
             </div>
             <div class="form-group">
                 <label>Intro</label>
-                <textarea class="form-control" rows="3" name="txtIntro"></textarea>
+                <textarea class="form-control" rows="3"
+                          name="txtIntro">{!! old('txtIntro',$product->intro) !!}</textarea>
+                <script>ckeditor('txtIntro')</script>
             </div>
             <div class="form-group">
                 <label>Content</label>
-                <textarea class="form-control" rows="3" name="txtContent"></textarea>
+                <textarea class="form-control" rows="3"
+                          name="txtContent">{!! old('txtContent',$product->content) !!}</textarea>
+                <script>ckeditor('txtContent')</script>
+
+            </div>
+            <div class="form-group">
+                <label>Images Current</label>
+                <img src="{!! asset('resources/upload/'.$product->image) !!}" alt="{!! $product->image !!}"
+                     class="img-responsive img-thumbnail img-thumbnail-current">
             </div>
             <div class="form-group">
                 <label>Images</label>
@@ -27,24 +55,29 @@
             </div>
             <div class="form-group">
                 <label>Product Keywords</label>
-                <input class="form-control" name="txtOrder" placeholder="Please Enter Category Keywords"/>
+                <input class="form-control" name="txtKeywords" placeholder="Please Enter Category Keywords"
+                       value="{!! old('txtKeywords',$product->keywords) !!}"/>
             </div>
             <div class="form-group">
                 <label>Product Description</label>
-                <textarea class="form-control" rows="3"></textarea>
-            </div>
-            <div class="form-group">
-                <label>Product Status</label>
-                <label class="radio-inline">
-                    <input name="rdoStatus" value="1" checked="" type="radio">Visible
-                </label>
-                <label class="radio-inline">
-                    <input name="rdoStatus" value="2" type="radio">Invisible
-                </label>
+                <textarea class="form-control" rows="3"
+                          name="txtDescription">{!! old('txtDescription',$product->description) !!}</textarea>
             </div>
             <button type="submit" class="btn btn-default">Product Edit</button>
             <button type="reset" class="btn btn-default">Reset</button>
-        </form>
-    </div>
+            <input type="hidden" name="_token" value="{!! csrf_token() !!}">
 
+        </div>
+        <div class="col-lg-1"></div>
+        <div class="col-lg-4">
+            @foreach($productImages as $key => $productImage)
+                <div class="form-group" id="hinh{!! $key !!}">
+                    <img src="{!! asset('resources/upload/detail/'.$productImage->image) !!}"
+                         alt="{!! $productImage->image !!}"
+                         class="img-responsive img-thumbnail img-thumbnail-detail" id="hinh{!! $key !!}">
+                </div>
+            @endforeach
+        </div>
+        </div>
+    </form>
 @endsection
