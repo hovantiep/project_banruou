40d70c7dd4cb99b5eedb3e16e88162b98256ee3c
Van Tiep <hovantiep1989@gmail.com>
2016-04-15 16:33:03 +0700

    product edit ajax

--- public/admin/js/my-script.js
+++ public/admin/js/my-script.js
@@ -15,9 +15,59 @@ $('div.alert').delay(3000).slideUp();
 /**
  * Xác nhận xóa
  */
-function confirmDel(msg){
-    if(window.confirm(msg)){
+function confirmDel(msg) {
+    if (window.confirm(msg)) {
         return true;
     }
     return false;
 }
+/**
+ * Thêm hình ảnh cho sản phẩm: product/edit
+ */
+$(document).ready(function () {
+    $('#addImages').click(function () {
+        $('#insert').append('<div class="form-group"><input type="file" name="fEditDetail[]" class="file" </div>');
+    });
+});
+
+/**
+ * Xóa hình ảnh sản phẩm ajax: product/edit
+ */
+$(document).ready(function () {
+    $('a#del_img').on('click', function () {
+        var url = 'http://project_laravel.local/admin/product/delImg/';
+        var _token = $("form[name=frmEditProduct]").find('input[name=_token]').attr('value');
+        var src = $(this).parent().find('img').attr('src');
+        var idImg = $(this).parent().find('img').attr('id');
+        var groupId = $(this).parent().attr('id');
+        $.ajax({
+                url: url + idImg,
+                type: 'GET',
+                cache: false,
+                data: {"_token": _token, "idImg": idImg, "urlImg": src},
+                success: function (data) {
+                    if (data == "OK") {
+                        $('#' + groupId).remove();
+                    } else {
+                        alert("Error!");
+                    }
+                }
+            }
+        )
+        ;
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
